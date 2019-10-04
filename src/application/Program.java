10d67b6ec5122dcc8aba13eaebd0177b916f8e43
Thiago Azevedo <thiago7azevedo@gmail.com>
2019-10-04 11:37:09 -0300

    Insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -27,7 +28,18 @@ public class Program {
 		List<Seller> list = sellerDao.findByDepartmnet(department);
 		for (Seller obj : list) {
 			System.out.println(obj);
+		}
+			/*===== TEST 2: seller findByDepartment =====
+Seller [id=6, name=Alex Pink, email=bob@gmail.com, birthDate=1997-03-04, baseSalary=6000.0, department=Department [id=2, name=Electronics]]
+Seller [id=2, name=Maria Green, email=maria@gmail.com, birthDate=1979-12-31, baseSalary=6000.0, department=Department [id=2, name=Electronics]]* /
+		*/
+	
+		System.out.println("\n===== TEST 3: seller findAll =====");
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
 			/*
+			 /*
 			 * ===== TEST 3: seller findAll =====
 			 * Seller [id=3, name=Alex Grey, email=alex@gmail.com, birthDate=1988-01-15, baseSalary=5000.0, department=Department [id=1, name=Computers]]
 			 * Seller [id=6, name=Alex Pink, email=bob@gmail.com, birthDate=1997-03-04, baseSalary=6000.0, department=Department [id=2, name=Electronics]]
@@ -38,21 +50,13 @@ public class Program {
 			 * Seller [id=4, name=Martha Red, email=martha@gmail.com, birthDate=1993-11-30, baseSalary=3000.0, department=Department [id=4, name=Books]]
 			 * Seller [id=8, name=Thiago Azevedo, email=thiago7azevedo@gmail.com, birthDate=1987-03-15, baseSalary=5000.0, department=Department [id=1, name=Computers]]
 			 * Seller [id=9, name=Thiago Azevedo, email=thiago7azevedo@gmail.com, birthDate=1987-03-15, baseSalary=5000.0, department=Department [id=1, name=Computers]]
-			 ** /
+			 **/
 		}
-	
-		System.out.println("\n===== TEST 3: seller findAll =====");
-		list = sellerDao.findAll();
-		for (Seller obj : list) {
-			System.out.println(obj);
-			/*
-			 * ===== TEST 2: seller findByDepartment =====
-			 * Seller [id=6, name=Alex Pink, email=bob@gmail.com, birthDate=1997-03-04, baseSalary=6000.0, department=Department [id=2, name=Electronics]]
-			 * Seller [id=2, name=Maria Green, email=maria@gmail.com, birthDate=1979-12-31, baseSalary=6000.0, department=Department [id=2, name=Electronics]]
-			 */
-		}
-		
-		
+
+		System.out.println("\n===== TEST 4: seller insert =====");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
 	}
 
 }
